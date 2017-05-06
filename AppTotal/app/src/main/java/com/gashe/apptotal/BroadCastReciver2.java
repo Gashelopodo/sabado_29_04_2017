e36b4a236c4bf86e7b3dc61d0836add119a4c330
cice <cice@aula8PovMacN09.local>
2017-05-06 12:09:03 +0200

    v3

--- AppTotal/app/src/main/java/com/gashe/apptotal/BroadCastReciver2.java
+++ AppTotal/app/src/main/java/com/gashe/apptotal/BroadCastReciver2.java
@@ -3,13 +3,24 @@ package com.gashe.apptotal;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class BroadCastReciver2 extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        try
+        {
+            Intent intent_serv = new Intent(context, ChequeoService.class);
+
+            context.startService(intent_serv);
+        }
+        catch (Throwable t)
+        {
+            Log.e(getClass().getCanonicalName(), "ERRORAZO", t);
+        }
+
+
     }
 }
